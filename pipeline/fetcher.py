762132b6c14c7941e7592e5bd604e37805f635b2
Fooxii <ironata07@gmail.com>
2026-06-22 19:05:05 -0400

    Implemented an early version of the fetcher.py file, it fetches for a test source list with only one source to fetch from.
    Source list external file will be added in the future

--- pipeline/fetcher.py
+++ pipeline/fetcher.py
@@ -1,12 +1,44 @@
+#
+# THE FETCHER FILE IS USED TO FETCH DATA FROM A LIST OF SOURCES
+#
+
 import feedparser
+from bs4 import BeautifulSoup
+# import source list from external file
 
-source_list = ['https://www.pcgamer.com/rss.xml']
+# Temp source list for testing
+source_list = ['https://www.pcgamer.com/feeds.xml']
 
+# feed_all is list containing rss output of each source
 feed_all = []
+
+# for every source in the source list a feed is created
 for source in source_list:
   feed = feedparser.parse(source)
+  # for every entry in each of the feeds data will be saved for future use in the pipeline
   for item in feed.entries:
-    feed_all.append(f'{item.title} \n')
+    raw_html = None
+    for content_item in item.content:
+      if content_item.get('type') == 'text/html':
+        raw_html = content_item['value']
+        break
 
-for item in feed_all:
-  print(item)
+    if raw_html is None:
+      raw_html = item.summary
+    # uses BeautifulSoup to parse the raw_html returned and only returns text inside p tags
+    soup = BeautifulSoup(raw_html, 'html.parser')
+
+    paragraphs = soup.find_all('p')
+    clean_text = ' '.join(p.get_text(strip=True) for p in paragraphs)
+
+    # ------------ FOR TESTING ------------------
+    # print(item.title)
+    # print(item.author)
+    # print(item.link)
+    # print(item.media_thumbnail[0]['url'])
+    # print(clean_text)
+
+    title = item.title
+    author = item.author
+    link = item.link
+    thumbnail = item.media_thumbnail[0]['url']
