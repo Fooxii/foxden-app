38624ed34b57126a8ccefd590a9a64279dc25b13
Fooxii <ironata07@gmail.com>
2026-06-24 15:19:25 -0400

    Fixed up the fetcher file to be wrapped under a function and return the list of entries per source

--- pipeline/fetcher.py
+++ pipeline/fetcher.py
@@ -1,44 +1,50 @@
-#
 # THE FETCHER FILE IS USED TO FETCH DATA FROM A LIST OF SOURCES
-#
-
 import feedparser
-from bs4 import BeautifulSoup
-# import source list from external file
+#+import source list from external file
 
-# Temp source list for testing
+#-Temp source list for testing
 source_list = ['https://www.pcgamer.com/feeds.xml']
+def fetch_all_sources():
+  # list containing rss output of each source
+  feed_all = []
 
-# feed_all is list containing rss output of each source
-feed_all = []
+  # iterates through each source
+  for source in source_list:
+    # feed is the entire xml page
+    feed = feedparser.parse(source)
+    # iterates through each article
+    for item in feed.entries:
+      raw_html = None
+      for content_item in item.content:
+        if content_item.get('type') == 'text/html':
+          raw_html = content_item['value']
+          if not item.get('id'):
+              guid = item.link
+          else:
+              guid = item.id
+          title = item.title
+          author = item.author
+          link = item.link
+          thumbnail = item.media_thumbnail[0]['url']
+          published = item.published
 
-# for every source in the source list a feed is created
-for source in source_list:
-  feed = feedparser.parse(source)
-  # for every entry in each of the feeds data will be saved for future use in the pipeline
-  for item in feed.entries:
-    raw_html = None
-    for content_item in item.content:
-      if content_item.get('type') == 'text/html':
-        raw_html = content_item['value']
-        break
+          entry = {
+            "guid": guid,
+            "title": title,
+            "author": author,
+            "link": link,
+            "thumbnail": thumbnail,
+            "raw_html": raw_html,
+            "published": published
+          }
 
-    if raw_html is None:
-      raw_html = item.summary
-    # uses BeautifulSoup to parse the raw_html returned and only returns text inside p tags
-    soup = BeautifulSoup(raw_html, 'html.parser')
+          feed_all.append(entry)
 
-    paragraphs = soup.find_all('p')
-    clean_text = ' '.join(p.get_text(strip=True) for p in paragraphs)
+  return feed_all
 
-    # ------------ FOR TESTING ------------------
-    # print(item.title)
-    # print(item.author)
-    # print(item.link)
-    # print(item.media_thumbnail[0]['url'])
-    # print(clean_text)
-
-    title = item.title
-    author = item.author
-    link = item.link
-    thumbnail = item.media_thumbnail[0]['url']
+      # ------------ FOR TESTING ------------------
+      # print(item.title)
+      # print(item.author)
+      # print(item.link)
+      # print(item.media_thumbnail[0]['url'])
+      # print(clean_text)
