5389d55bf24027ebfe0064b1dcbd9478efb39a38
Fooxii <ironata07@gmail.com>
2026-06-29 20:05:26 -0400

    Finished implementing the filter duplicate file that will check the database source urls to see if the article being processed has already been processed

---
+++ pipeline/dup_filter.py
@@ -0,0 +1,16 @@
+# THE DUP_FILTER FILE WILL COMPARE INCOMING URLS WITH THOSE IN THE DATABASE, FILTERING OUT DUPLICATES
+from normalizer import normalize_all
+from supabase_client import supabase
+
+def filter_duplicates(normalized):
+  article_urls = supabase.table("articles").select("url").execute()
+  
+  existing_urls = {row["url"] for row in article_urls.data}
+
+  filtered_entries = []
+
+  for entry in normalized:
+    if entry["link"] not in existing_urls:
+      filtered_entries.append(entry)
+
+  return filtered_entries
