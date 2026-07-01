2e681a4024bcb36a34457621420b712cb78f25cd
Fooxii <ironata07@gmail.com>
2026-06-30 22:41:50 -0400

    Added sentence-transformer to the requirements file
    Implemented the embedding generator file to generate vector embeddings for both tags and articles

---
+++ pipeline/embedding_generator.py
@@ -0,0 +1,20 @@
+# THE EMBEDDING GENERATOR FILE WILL GENERATE VECTOR EMBEDDINGS FOR ARTICLES AND TAGS USING SENTENCE TRANSFORMERS
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+def generate_embedding(text):
+  if not text:
+    return None
+
+  embedding = model.encode(text)
+  return embedding.tolist()
+
+def generate_article_embedding(article):
+  text = f"{article['title']} {article['content']}"
+  return generate_embedding(text)
+
+def generate_tag_embedding(tag):
+  keywords = ' '.join(tag['keywords']) if tag.get('keywords') else ''
+  text = f"{tag['name']} {keywords}"
+  return generate_embedding(text)
